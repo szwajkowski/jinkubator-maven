c27e706796f2cf03d5c0988a54113e56f734cdbb
Lukasz Szwajkowski <lukasz.szwajkowski@allegrogroup.com>
2017-01-30 23:08:10 +0100

    step03 - dependencies and jar with deps

---
+++ step03/jinkubator-maven/src/main/java/pl/poznan/jug/jinkubatormaven/Sample.java
@@ -0,0 +1,12 @@
+package pl.poznan.jug.jinkubatormaven;
+
+public class Sample {
+
+    private final String a;
+    private final String b;
+
+    public Sample(String a, String b) {
+        this.a = a;
+        this.b = b;
+    }
+}
